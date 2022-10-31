b3b4c3a754f22464bc829e66c5e90fccad12d9e0
Prashant Gupta <prashant05k@gmail.com>
2022-10-31 19:38:49 +0530

    New Changes

--- public/Code.js
+++ public/Code.js
@@ -1,42 +1,9 @@
-let datas = [
-  { name: "Ajay", profession: "Engr" },
-  { name: "Vivek", profession: "Engr" },
-];
+let str = "abc";
 
-function getdatas() {
-  setTimeout(() => {
-    datas.forEach((data) => console.log(data.name));
-  }, 500);
+function permutations(str) {
+ for(let i=0; i<str.length; i++){
+    
+ }
 }
-
-function createdata(newdata) {
-  return new Promise((resolve, reject) => {
-    if (1) {
-      setTimeout(() => {
-        datas.push(newdata);
-        resolve();
-      }, 2000);
-    } else {
-      reject(console.log("Failure"));
-    }
-  });
-}
-
-async function start() {
-  await createdata({ name: "Prashant", profession: "Engr" });
-  console.log("run first");
-  getdatas();
-}
-
-start();
-
-// async function start() {
-//   try {
-//     await createdata({ name: "Prashant", profession: "Engr" });
-//     getdatas();
-//   } catch {
-//     (e) => console.log(e);
-//   } finally {
-//     console.log("Finally");
-//   }
-// }
+const res = permutations(str);
+console.log(res, res.length);
